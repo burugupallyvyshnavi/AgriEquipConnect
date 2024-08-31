a8c1a94f4b0573035239a3bfd5aea6a984423e7c
burugupallyvyshnavi <135447120+burugupallyvyshnavi@users.noreply.github.com>
2024-08-31 11:34:55 +0530

    Add files via upload

---
+++ agri.js
@@ -0,0 +1,10 @@
+// Basic script to handle smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
